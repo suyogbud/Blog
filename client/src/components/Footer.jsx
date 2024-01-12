15e9c8662f25e9b36a9548f6fd7ae729aa9fb273
suyogbudg <suyogbc10@gmail.com>
2024-01-11 20:47:18 -0500

    Made some basic nav and footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <div className="logo">Suyog's Blog</div>
+      <span>Made by Suyog.</span>
+    </footer>
+  );
 };
 
 export default Footer;
