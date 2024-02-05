c62bbe85ae8f8fd59c0284c5ea398ed6f340f7cd
Suyog Singh Budhathoki <suyogbc10@gmail.com>
2024-02-04 23:30:17 -0500

    Started changes in writing page

--- client/src/pages/Write.jsx
+++ client/src/pages/Write.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Write = () => {
-  return <div>Write</div>;
+  return (
+    <div className="add">
+      <div className="content">
+        <div className="menu">
+          <div className="item"></div>
+          <div className="item"></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Write;
