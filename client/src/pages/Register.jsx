14a7884d0eb522518fa3d94fda4b5c08539cf070
Suyog <suyogbc10@gmail.com>
2024-02-08 00:13:14 -0500

    Made Some changes to Login and Register page

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,18 +1,62 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Register = () => {
+  const [inputs, setInputs] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  const [err, setError] = useState(null);
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("/auth/register", inputs);
+      navigate("/login");
+    } catch (err) {
+      setError(
+        err.response?.data?.message || JSON.stringify(err.response?.data)
+      );
+    }
+  };
+
   return (
     <div className="auth">
-      <h1>Sign-Up for an Account</h1>
-      <form>
-        <input required type="text" placeholder="username" />
-        <input required type="email" placeholder="email" />
-        <input required type="password" placeholder="password" />
-        <button>Sign-Up</button>
-        <p></p>
+      <h1>Register</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          required
+          type="text"
+          placeholder="username"
+          name="username"
+          onChange={handleChange}
+        />
+        <input
+          required
+          type="email"
+          placeholder="email"
+          name="email"
+          onChange={handleChange}
+        />
+        <input
+          required
+          type="password"
+          placeholder="password"
+          name="password"
+          onChange={handleChange}
+        />
+        <button type="submit">Register</button>
+        {err && <p>{err}</p>}
         <span>
-          Already have an account? <Link to={"/login"}>Login</Link>
+          Do you have an account? <Link to="/login">Login</Link>
         </span>
       </form>
     </div>
